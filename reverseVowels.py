ae37236327c82238ed182acdf8830aa64d65c99a
Mithun Bharadhwaj <mithunbharadhwaj10@gmail.com>
2025-10-20 12:39:37 +0530

    Initial commit

--- reverseVowels.py
+++ reverseVowels.py
@@ -19,4 +19,4 @@ class Solution1:
 s= Solution1()
 print(f"'IceCreAm' -> '{s.reverseVowels('IceCreAm')}'")
 print(f"'leetcode' -> '{s.reverseVowels('leetcode')}'")
-print(f"'hello' -> '{s.reverseVowels('hello')}'")  # Test change
+print(f"'hello' -> '{s.reverseVowels('Hello')}'") 
