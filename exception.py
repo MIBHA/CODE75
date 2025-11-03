8b57ab71eac79333939675499d66916ba1ae6406
Mithun Bharadhwaj <mithunbharadhwaj10@gmail.com>
2025-11-03 10:05:30 +0530

    Day11 commit

---
+++ exception.py
@@ -0,0 +1,15 @@
+file = None
+try:
+    file = open("file.txt", "r")
+    data = file.read()
+    print(data)
+except FileNotFoundError:
+    print("File not found")
+except PermissionError:
+    print("Permission denied")
+except Exception as e:
+    print("An error occurred:", e)
+finally:
+    if file is not None:
+        file.close()
+        print("File closed")
